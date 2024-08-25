ae56cf6b0becb9ef1cc47d856fc4b30822541c75
vadivel-tellis <vadivel@tellistechnologies.com>
2024-08-25 23:19:38 +0530

    fixed token

--- blog/src/utils/AxiosInstance.js
+++ blog/src/utils/AxiosInstance.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const { token } = JSON.parse(localStorage.getItem("user")) || null;
-console.log(token);
+const user = JSON.parse(localStorage.getItem("user")) || null;
 
 const AxiosInstance = axios.create({
   baseURL: "https://mern-blog-9kew.onrender.com/api/",
-  headers: { Authorization: `Bearer ${token}` },
+  headers: { Authorization: `Bearer ${user?.token}` },
 });
 
 export default AxiosInstance;
