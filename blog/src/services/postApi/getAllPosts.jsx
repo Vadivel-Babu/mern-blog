ab4bb7f0fa19785e7a760c1c0f5960f3c1e9331a
vadivel-tellis <vadivel@tellistechnologies.com>
2024-08-25 23:11:13 +0530

    fixed build error

--- blog/src/services/postApi/getAllPosts.jsx
+++ blog/src/services/postApi/getAllPosts.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import AxiosInstance from "../../utils/axiosInstance";
+import AxiosInstance from "../../utils/AxiosInstance";
 
 const getPosts = async () => {
   const response = await AxiosInstance.get("posts");
