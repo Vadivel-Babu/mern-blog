3228bb1ddbeecc4fba22726d6d5cc69df50e0c35
Vadivel-Babu <vadivelbabu1996@gmail.com>
2024-10-05 16:19:36 +0530

    added

--- blog/src/components/Footer.jsx
+++ blog/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-blue text-white p-5 text-center">
-      <p> @ {new Date().getFullYear()} copyright to new blog.</p>
+      <p> @ {new Date().getFullYear()} copyright to blog.</p>
     </footer>
   );
 };
