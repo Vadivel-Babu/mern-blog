d2f74c301bcdb92dc1f404063727ebcc1f9aeb19
Vadivel-Babu <vadivelbabu1996@gmail.com>
2024-11-20 21:52:10 +0530

    fixed the cors error

--- blogserver/index.js
+++ blogserver/index.js
@@ -18,7 +18,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 connectDB();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://mern-blog-9kew.onrender.com", credentials: true })
+);
 app.use(json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
